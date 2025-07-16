a345ba892e0fe306bb3263131016478e6202835e
dmy <dimonshulman2@gmail.com>
2025-07-16 13:18:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,11 @@
-const staticCacheName = 's-app-v1';
+self.addEventListener('install', (event) => {
+	console.log('Установлен');
+});
 
-const assetUrls = [
-    '/',
-    '/index.html',
-    '/css/styles.css',
-    '/img/umbrella_logo.png',
-    '/img/spencer.png',
-    '/contacts.html'
-];
+self.addEventListener('activate', (event) => {
+	console.log('Активирован');
+});
 
-self.addEventListener('install', async event => {
-    const cache = await caches.open(staticCacheName);
-    await cache.addAll(assetUrls);
-});  
-
-self.addEventListener('activate', event => {
-    console.log('Service Worker activated');
-}); 
-
-self.addEventListener('fetch', event => {
-    console.log('Fetch', event.request.url);
-    event.respondWith(cacheFirst(event.request));
-}); 
-
-async function cacheFirst(request) {
-    const cached = await caches.match(request);
-    return cached ?? await fetch(request);
-}
+self.addEventListener('fetch', (event) => {
+	console.log('Происходит запрос на сервер');
+});

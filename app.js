f9ef5966b72731c423a8d28cc08600d0c537c44a
mitruee <dimonshulman2@gmail.com>
2025-07-16 12:53:57 +0300

    sw.js edited

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-        .then(registration => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-            console.error('Service Worker registration failed:', error);
-        });
-    });
-}
+const CACHE_NAME = 'umbrella-corp-v1';
+
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll([
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/img/umbrella_logo.png',
+                '/img/spencer.png'
+            ]))
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});

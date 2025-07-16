68277c8b3ccbf07dddc509aee377c8afad016ee2
mitruee <dimonshulman2@gmail.com>
2025-07-16 13:02:48 +0300

    sw.js edited

--- sw.js
+++ sw.js
@@ -1,10 +1,26 @@
-const CACHE_NAME = 'umbrella-corp-v1';
+const staticCacheName = 's-app-v1';
 
-self.addEventListener('install', event => {
-    console.log('Service Worker installing.');
-});
+const assetUrls = [
+    'index.html',
+    'app.js',
+    'style.css'
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(assetUrls);
+});  
 
 self.addEventListener('activate', event => {
-    console.log('Service Worker activating.');
-});
+    console.log('Service Worker activated');
+}); 
 
+self.addEventListener('fetch', event => {
+    console.log('Fetch', event.request.url);
+    event.respondWith(cacheFirst(event.request));
+}); 
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request);
+    return cached ?? await fetch(request);
+}
